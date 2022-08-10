002bbf637c2d96a05b9f60104d43c8a4ecfd22aa
jzabar <73255741+JaydanZ@users.noreply.github.com>
2022-08-09 21:12:01 -0400

    Fixed netlify form 404 error

--- src/Components/Contact/ContactBody.js
+++ src/Components/Contact/ContactBody.js
@@ -136,7 +136,7 @@ const ContactBody = () => {
                   method="POST"
                   className="contact_card_body-form"
                 >
-                  <input type="hidden" name="contact" value="contact form" />
+                  <input type="hidden" name="form-name" value="contact form" />
                   <input type="text" placeholder="Name" name="name" />
                   <input type="email" placeholder="Email" name="email" />
                   <textarea placeholder="Message" name="message" />
