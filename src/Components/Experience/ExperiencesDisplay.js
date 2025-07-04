d6afced81bc88552ec7283dae0c8c1b76936a013
jzabar <73255741+JaydanZ@users.noreply.github.com>
2025-07-03 21:27:52 -0400

    updated job path bar design for responsiveness

--- src/Components/Experience/ExperiencesDisplay.js
+++ src/Components/Experience/ExperiencesDisplay.js
@@ -4,10 +4,13 @@ import ExperienceBody from "./ExperienceBody";
 const ExperiencesDisplay = (props) => {
   return (
     <div className="experiences_display_container">
-      <div className="job_container">
-        {props.experiences.map((job) => (
-          <ExperienceBody job={job} />
-        ))}
+      <div className="job_container_boundary">
+        <div className="job_path_bar"></div>
+        <div className="job_container">
+          {props.experiences.map((job) => (
+            <ExperienceBody job={job} />
+          ))}
+        </div>
       </div>
     </div>
   );
