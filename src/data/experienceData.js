166d698fdeb5f15c3710ce50a236a101060c899e
jzabar <73255741+JaydanZ@users.noreply.github.com>
2025-08-25 18:55:54 -0400

    more experience data updates

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -4,12 +4,21 @@ const experienceData = [
     jobRole: "Software Developer",
     startDate: "Jan 2023",
     endDate: "June 2025",
-    technologies: ["React", "Django", "PostgreSQL", "Firebase", "AWS"],
+    technologies: [
+      "Javascript",
+      "React",
+      "Redux",
+      "SCSS",
+      "Django",
+      "PostgreSQL",
+      "Firebase",
+      "AWS",
+    ],
     acomplishments: [
       "Designed and developed a Personnel Data Reporting Interface using Javascript, React, Redux, SCSS, Django and Firebase, streamlining employee data management for customers and significantly reducing manual administrative workload by automating key data tracking processes",
       "Improved load times by up to 40% by refactoring frontend codebase, improving react component efficiency with hooks, memoization and package migrations, reducing unnecessary re-renders and enhancing overall application performance",
       "Implemented unit and integration tests using Jest (frontend) and Pytest (backend), increasing code coverage up to 70%, ensuring performance and reliability of JCAII’s Icelink platform and preventing regressions across multiple feature releases",
-      "Optimized backend performance using Django and PostgreSQL by enhancing API endpoints and database queries, reducing response times and improving scalability under high-load conditions",
+      "Optimized backend performance using Django and PostgreSQL by enhancing REST API endpoints and database queries, reducing response times and improving scalability under high-load conditions",
       "Reduced system downtime for over 5,000 active users by analyzing AWS Codebuild error logs and resolving critical production issues in Icelink’s AWS S3 file processing pipelines",
     ],
     mobileAcomplishments: [
