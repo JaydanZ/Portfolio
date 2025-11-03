75d78bf1b67edec18e868bfb442bc9f6420bb9d7
jzabar <73255741+JaydanZ@users.noreply.github.com>
2025-11-02 23:21:45 -0500

    Updated devicon version, removed animation for skill icons, adjusted styling

--- src/Components/Skills/SkillsCard.js
+++ src/Components/Skills/SkillsCard.js
@@ -1,40 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import "./SkillsCard.css";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const SkillsCard = (props) => {
-  let skillItemRef = useRef();
-
-  useEffect(() => {
-    // Register gsap pluggin
-    gsap.registerPlugin(ScrollTrigger);
-
-    const skillItemTl = gsap.timeline({
-      scrollTrigger: {
-        trigger: skillItemRef,
-      },
-    });
-
-    skillItemTl.fromTo(
-      skillItemRef,
-      {
-        y: 40,
-        autoAlpha: 0,
-      },
-      {
-        y: 0,
-        autoAlpha: 1,
-        duration: 0.5,
-        delay: 0.8,
-        ease: "power1-out",
-      }
-    );
-  }, []);
   return (
     <div className="skills_card">
       <h2>{props.data.category}</h2>
-      <div className="skills_container" ref={(el) => (skillItemRef = el)}>
+      <div className="skills_container">
         {props.data.skills.map((entry) => (
           <div className="skill_item">
             <div className="skill_img">
