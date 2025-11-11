28e532ecf7c2b5fc5b2148eafb1a58f8d4168c9b
jzabar <73255741+JaydanZ@users.noreply.github.com>
2025-11-11 14:57:38 -0500

    Updated job data

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,11 +1,11 @@
 const experienceData = [
   {
-    jobTitle: "JCAII",
+    jobTitle: "JCAI Inc.",
     jobRole: "Software Developer",
     startDate: "Jan 2023",
     endDate: "June 2025",
     technologies: [
-      "Javascript",
+      "Typescript",
       "React",
       "Redux",
       "SCSS",
@@ -18,7 +18,7 @@ const experienceData = [
       "Kubernetes",
     ],
     acomplishments: [
-      "Developed a Personnel Data Reporting Interface using Javascript, React, Redux, SCSS, Django and Firebase to automate key personnel data tracking processes, reducing manual customer data input by 50%",
+      "Developed a Personnel Data Reporting Interface using Typescript, React, Redux, SCSS, Django and Firebase to automate key personnel data tracking processes, reducing manual customer data input by 50%",
       "Modernized a real-time video feed aircraft detection application by utilizing Docker for deployment and implementing WebSockets using a Node.js server, increasing detection data processing between services by 80%",
       "Decreased application load times by 40% by refactoring frontend codebase, improving react component efficiency with custom hooks, memoization and package / library migrations, reducing unnecessary re-renders",
       "Prevented regressions across multiple deployments by implementing robust unit and integration tests using Jest (frontend) and Pytest (backend), increasing code coverage up to 90%",
@@ -26,7 +26,7 @@ const experienceData = [
       "Collaborated with cross-functional developers to debug and resolve production outages in Icelink's AWS S3 file processing pipelines, leveraging AWS CodeBuild diagnostics to rapidly restore uptime for 10,000+ users",
     ],
     mobileAcomplishments: [
-      "Developed a Personnel Data Reporting Interface using Javascript, React, Redux, SCSS, Django and Firebase to automate key personnel data tracking processes, reducing manual customer data input by 50%",
+      "Developed a Personnel Data Reporting Interface using Typescript, React, Redux, SCSS, Django and Firebase to automate key personnel data tracking processes, reducing manual customer data input by 50%",
       "Modernized a real-time video feed aircraft detection application by utilizing Docker for deployment and implementing WebSockets using a Node.js server, increasing detection data processing between services by 80%",
       "Decreased application load times by 40% by refactoring frontend codebase, improving react component efficiency with custom hooks, memoization and package / library migrations, reducing unnecessary re-renders",
       "Prevented regressions across multiple deployments by implementing robust unit and integration tests using Jest (frontend) and Pytest (backend), increasing code coverage up to 90%",
